d7849cf983ccb021ff70bf2dce9e7c84278543b9
杨超俊 <yangchaojunxs@126.com>
2022-01-21 14:15:51 +0800

    Profile Page, Register Page

--- social-app/front-end/src/App.js
+++ social-app/front-end/src/App.js
@@ -1,8 +1,10 @@
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
+import Register from './pages/register/Register'
+import Profile from './pages/profile/Profile'
 
 function App() {
-  return <Login />
+  return <Profile />
 }
 
 export default App
